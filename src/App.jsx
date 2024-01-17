1d96df50810f3c94319ae28ffd795c334a030880
Kristine <kbermoy02@gmail.com>
2024-01-17 15:49:39 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
