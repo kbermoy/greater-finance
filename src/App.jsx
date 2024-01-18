75eca8e39df172b0e3aaf72875e6a8f3dd0fd584
Kristine <kbermoy02@gmail.com>
2024-01-18 12:37:43 -0800

    completed cloud UI for welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './components/Welcome';
+import Cloud from './components/UI/Cloud';
 
 function App() {
+  const cloudColors = [
+    {front: 'white'},
+    {mid: 'lightPurple'},
+    {back: 'darkPurple'}
+  ]
+
   return (
     <Router>
-      
+      {/* <Welcome /> */}
+      <Cloud cloudColors={cloudColors} />
     </Router>
   );
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="welcome__body">
+      <div className="welcome__description">
+        <h1>Welcome To Logo</h1>
+        <h2>Logo is a platform to manage your finances in one place</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
